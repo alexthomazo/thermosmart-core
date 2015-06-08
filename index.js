878f374b435c90eb040fe5d491edd2d82678f4e7
Alexandre Thomazo <alex@thomazo.info>
2015-06-08 15:08:57 +0200

    handle temperature message

--- index.js
+++ index.js
@@ -1,5 +1,39 @@
-var fs = require('fs');
+var fs = require('fs'),
+    mqtt = require('mqtt');
 
-var conf = JSON.parse(fs.readFileSync('conf.json'));
+var mqtt_base_topic = 'thermosmart/';
+var conf = JSON.parse(fs.readFileSync('config.json'));
 
-console.log(conf);
+//setup mqtt communication
+var client  = mqtt.connect('mqtt://localhost');
+
+var sensorService = require('./lib/SensorService.js')(conf);
+
+// ----------------------------------------------------------------------------------
+// --------------                  MQTT PART                   ----------------------
+// ----------------------------------------------------------------------------------
+client.on('connect', function () {
+    console.log("MQTT Connected");
+    monitor();
+
+    //read previous state persisted into mqtt broker
+    client.subscribe(mqtt_base_topic + 'temperature/#');
+});
+
+
+client.on('message', function (topic, message) {
+    var msg = JSON.parse(message.toString());
+    sensorService.handleMsg(topic, msg);
+});
+
+function monitor() {
+    //publish each 30s that the server is still alive
+    client.publish("thermosmart/monitor/core", JSON.stringify(new Date().toISOString()), { retain: true });
+    setTimeout(monitor, 30000);
+}
+
+function printSensors() {
+    console.log(sensorService.getList());
+    setTimeout(printSensors, 5000);
+}
+printSensors();
--- tests/SensorServiceTest.js
+++ tests/SensorServiceTest.js
@@ -4,32 +4,15 @@ var sensorSrvFact = require('../lib/SensorService.js');
 
 describe('SensorService', function() {
 
-	it('update new value', function(done) {
-		var sensorSrv = sensorSrvFact();
+	it('handle message', function(done) {
+		var sensors = [{ "id": "jee-chambre", "name": "Chambre", "type": "jeenode", "sensorId": "80" }];
+		var sensorSrv = sensorSrvFact({ sensors: sensors });
 
-		sensorSrv.update('jee-80', {t: 20.5});
+		sensorSrv.handleMsg('thermosmart/temperature/80', {t: 20.5, d: "2015-06-08T19:00:34.209Z"});
 
-		sensorSrv.getList().should.have.property('jee-80').with.property('t', 20.5);
-		done();
-	});
-
-	it('update existing value', function(done) {
-		var sensorSrv = sensorSrvFact();
-
-		sensorSrv.update('jee-80', {t: 20.5});
-		sensorSrv.update('jee-80', {t: 22.5});
-
-		sensorSrv.getList().should.have.property('jee-80').with.property('t', 22.5);
-		done();
-	});
-
-	it('dalete existing value', function(done) {
-		var sensorSrv = sensorSrvFact();
-
-		sensorSrv.update('jee-80', {t: 20.5});
-		sensorSrv.update('jee-80', null);
-
-		sensorSrv.getList().should.have.not.property('jee-80');
+		var sensor = sensorSrv.getList()[0];
+		sensor.should.have.property('t', 20.5);
+		sensor.should.have.property('d',  "2015-06-08T19:00:34.209Z");
 		done();
 	});
 });
